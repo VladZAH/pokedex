f8604051b0c333da142368ed965d649c22ca228c
Volodymyr Zahorodnyy <vovazahorodniy@gmail.com>
2021-06-05 21:37:33 -0700

    Added Project Structure like React Components and npm dependencies Axios and Emotion

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div> 
+            Pokemon Card
+        </div>
+    );
+} 
+
+export default Card;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+import Axios from 'axios';
+
+const Navbar = () => {
+
+    return(
+        <div>NavBar-SearchBar</div>
+    );
+}
+
+export default Navbar;
