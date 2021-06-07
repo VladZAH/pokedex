ecbffde9ab5c487d43c25a155acb48d661e6e73d
Volodymyr Zahorodnyy <vovazahorodniy@gmail.com>
2021-06-07 09:24:38 -0700

    Added advanced styles, diagrams, sprites view and autocomplete.

--- src/components/Navbar/Card/Card.js
+++ src/components/Navbar/Card/Card.js
@@ -1,63 +1,118 @@
 import React from 'react';
 import './Card.css';
 
-
 const Card = (props) => {
 
-const colours = {
-	normal: '#A8A77A',
-	fire: '#EE8130',
-	water: '#6390F0',
-	electric: '#F7D02C',
-	grass: '#7AC74C',
-	ice: '#96D9D6',
-	fighting: '#C22E28',
-	poison: '#A33EA1',
-	ground: '#E2BF65',
-	flying: '#A98FF3',
-	psychic: '#F95587',
-	bug: '#A6B91A',
-	rock: '#B6A136',
-	ghost: '#735797',
-	dragon: '#6F35FC',
-	dark: '#705746',
-	steel: '#B7B7CE',
-	fairy: '#D685AD',
-};
-const bgColor = colours[props.types[0]]
+    // colours for pokemon types
+    const colours = {
+        normal: '#A8A77A',
+        fire: '#EE8130',
+        water: '#6390F0',
+        electric: '#F7D02C',
+        grass: '#7AC74C',
+        ice: '#96D9D6',
+        fighting: '#C22E28',
+        poison: '#A33EA1',
+        ground: '#E2BF65',
+        flying: '#A98FF3',
+        psychic: '#F95587',
+        bug: '#A6B91A',
+        rock: '#B6A136',
+        ghost: '#735797',
+        dragon: '#6F35FC',
+        dark: '#705746',
+        steel: '#B7B7CE',
+        fairy: '#D685AD',
+    };
 
+    // colour of the card is determined by first type
+    const bgColor = colours[props.types[0]]
+
+    // destructuring basic stats array to variables for easier diagram implementation 
+    const hp = props.stats[0]
+    const atack = props.stats[1]
+    const defence = props.stats[2]
+    const spAtack = props.stats[3]
+    const spDefence = props.stats[4]
+    const speed = props.stats[5]
+
+    // handler for rendering sprites, abilities and types;
+    const renderSprites = props.sprites.map((s) => <img key={s} src={s} alt='/'></img>);
+    const renderAbilities = props.abilities.map((a) => <span className='abillity' key={a}>{a}</span>);
+    const renderTypes = props.types.map((t) => <span style={{backgroundColor: colours[t]}} key={t} className='type'>{t}</span>);
+
+    // for stats diagrams I used percentage bars and 240 is used because maximum stat ammount is 230
+    const AtackBar = {
+        width: `${Math.round(atack / 240 * 100)}%`
+    }
+    const DefenceBar = {
+        width: `${Math.round(defence / 240 * 100)}%`
+    }
+    const spAtackBar = {
+        width: `${Math.round(spAtack / 240 * 100)}%`
+    }
+    const spDefenceBar = {
+        width: `${Math.round(spDefence / 240 * 100)}%`
+    }
+    const SpeedBar = {
+        width: `${Math.round(speed / 240 * 100)}%`
+    }
+    
+    // setting gradient card background using colors from array
+    const FrameBackground = {
+        backgroundColor: bgColor, // for older browsers
+        background: `linear-gradient(to left, #F4E2D8, ${bgColor})`
+    }
+
+    // huge function that handles proper render of the card or 'not found message'
     const handleRender = () => {
-        if(props.name) {
-            const baseStats = props.stats.map((s) => <p className='stats' key={s.name}>{s.name}: {s.value}</p>); 
-            const renderSprites = props.sprites.map((s) => <img key={s} src={s} alt='/'></img>);
-            const renderAbilities = props.abilities.map((a) => <span key={a}>{a}, </span>);
-            const renderTypes = props.types.map((t) => <span style={{backgroundColor: colours[t]}} key={t} className='type'>{t}</span>);
+        if(props.name) {  
             return (
-                <div className='frame'>
+                <div className='frame' style={FrameBackground}>
                     <div>
                     <span className='name'>{props.name.toUpperCase()}</span>
-                    
+                    <span className='hp'>HP:{hp}</span>
                     {renderTypes}
                     </div>
                     <img className='mainImg'  src={props.image} alt='/'></img>
                     <div>{renderSprites}</div> 
                     <div className='info'>
-                        <p>Height: {props.height}" Weight: {props.weight}lb</p>
-                        <p>BASE STATS</p>
-                        <div>{baseStats}</div>
-                        <div>ABILITIES: {renderAbilities}</div>    
+                        <p className='heightWeight'>Height: {props.height}" ------  Weight: {props.weight} lb</p>
+                        <p className='stat'>Atack: {atack}</p>
+                        <div className="borderBar">
+                            <div className='progress' style={AtackBar}></div>
+                        </div>
+                        <p className='stat'>Defence: {defence}</p>
+                        <div className="borderBar">
+                            <div className='progress' style={DefenceBar}></div>
+                        </div>
+                        <p className='stat'>Special Atack: {spAtack}</p>
+                        <div className="borderBar">
+                            <div className='progress' style={spAtackBar}></div>
+                        </div>
+                        <p className='stat'>Special Defence: {spDefence}</p>
+                        <div className="borderBar">
+                            <div className='progress' style={spDefenceBar}></div>
+                        </div>
+                        <p className='stat'>Speed: {speed}</p>
+                        <div className="borderBar">
+                            <div className='progress' style={SpeedBar}></div>
+                        </div>
+                        
+                        <div className='abilitiesDiv'>ABILITIES: {renderAbilities}</div>    
                     </div>
 
                 </div>
             );
         } else {
             return (
-                <div>
-                    <p>Your pokemon in not found</p>
+                <div className='init'>
+                    Please type the name of the pokemon You wish to see and press 'search' or 'return' 
                 </div>
             );
         }
     }
+    // actual render of the card by calling function above
     return (
         <div> 
             {handleRender()}
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Axios from 'axios';
 import './Navbar.css';
 import Card from './Card/Card';
 
 
 const Navbar = () => {
-    
+    // defining state
+    const [notFound, setNotFound] = useState(false)
+    const [autocomplete, setAutocomplete] = (useState(false))
+    const [allPokemons, setAllPokemons] = useState([]);
+    const [allNames, setAllNames] = useState([]);
     const [searchName, setSearchName] = useState('');
     const [pokemonWeight, setPokemonWeight] = useState('');
     const [pokemonHeight, setPokemonHeight] = useState('');
@@ -15,7 +19,26 @@ const Navbar = () => {
     const [pokemonMainImg, setPokemonMainImg] = useState('');
     const [pokemonSprites, setSprites] = useState([]);
     const [pokemonAbilities, setPokemonAbilities] = useState([]);
+    // I was trying to implement rendering 20 random pokemons before any searches,
+    // but I ran out of time, this feature will be Implemented soon 
+    // const randomTwentyIDs = Array(20).fill().map(() => Math.round(Math.random() * 1117));
 
+
+    // load all pokemon names for autocomplete
+    useEffect(() => {
+        Axios.get('https://pokeapi.co/api/v2/pokemon?limit=1200')
+        .then(response => {
+           setAllPokemons(response.data.results)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }, []);
+
+
+
+
+    // fetching pokemon by name, then structuring and loading data to state
     const fetchPokemon = (name) => {
         Axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
         .then(response => {
@@ -30,7 +53,7 @@ const Navbar = () => {
                 types.push(t.type.name)
             });
             data.stats.forEach(s => {
-                stats.push({name: s.stat.name, value: s.base_stat})
+                stats.push(s.base_stat)
             });
 
             data.abilities.forEach(a => {
@@ -38,12 +61,13 @@ const Navbar = () => {
             });
 
             for(const p in data.sprites){
-                if(typeof data.sprites[p] === 'string'){
+                if(typeof data.sprites[p] === 'string' && sprites.length <= 5){
                     sprites.push(data.sprites[p])
                 }
             }
 
             setPokemonName(data.species.name);
+            // converting weight and height to inches and pounds 
             setPokemonWeight(Math.floor(data.weight * 0.22));
             setPokemonHeight(Math.floor(data.height * 3.93701));
             setPokemonStats(stats);
@@ -51,32 +75,64 @@ const Navbar = () => {
             setPokemonMainImg(data.sprites.other['official-artwork']['front_default']);
             setSprites(sprites)
             setPokemonAbilities(abilities)
+            // in case pokemon is not found
+            setNotFound(false)
 
         })
         .catch((error) => {
             setPokemonName('')
             console.log(error)
+            setNotFound(true)
         }) 
     }
 
+    // change input in case autocomplete is clicked
+    const changeInput = (e) => {
+        e.preventDefault()
+        setSearchName(e.currentTarget.textContent)
+        setAutocomplete(false)
+    }
+
+    // handle changes toinput and autocorrect updates
     const handleInput  = (e) => {
         setSearchName(e.target.value.toLowerCase());
+        let names = [];
+        allPokemons.forEach((p) => {
+            if(p.name.includes(searchName) && !p.name.includes('-')){
+                names.push(p.name)
+            }
+        })
+        if(names.length >= 1){
+            if(names.length >= 20){
+                names = names.slice(0, 20)
+            }
+            setAllNames(names)
+            setAutocomplete(true)
+        }
     }
     
-
+    // handles form submit and trigers API call
     const handleSubmit = (e) => {
         e.preventDefault();
         fetchPokemon(searchName)
+        setAutocomplete(false)
+        setSearchName('')
 
     }
 
     return(
         <div>
-            <form onSubmit={handleSubmit}>
+            <form className='formBar' onSubmit={handleSubmit}>
                 <input className='NavInput' onChange={handleInput} value={searchName} type='text'></input>
                 <button className='NavButton' type='submit'>search</button>
+                
+                    {autocomplete && <div className='AcDiv'>{allNames.map((name) => {
+                        return <div onClick={changeInput} className='autocomplete' value={name} key={name}>{name}</div>
+                    })}
+                    </div>}
             </form>
             <Card name={pokemonName} weight={pokemonWeight} height={pokemonHeight} types={pokemonTypes} stats={pokemonStats} abilities={pokemonAbilities} sprites={pokemonSprites} image={pokemonMainImg} />
+            {notFound && <p>Your pokemon in not found</p>}
         </div>
     );
 }
